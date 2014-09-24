99e948225a5be2b96d9f4f6b6359d02a63f06bf0
fersige <fernandoromero99@hotmail.com>
2014-09-24 15:41:33 -0300

    agrego archivo info_curso

--- application/controllers/docente.php
+++ application/controllers/docente.php
@@ -7,14 +7,14 @@ class Docente extends CI_Controller {
 		$this->load->view('header');
 		$this->load->view('menuUp');
 
-		$this->load->model(array('Teacher_Model','Curso_Model'));
+		/*$this->load->model(array('Teacher_Model','Curso_Model'));
 
 		/*$query = $this->Teacher_Model->get_asignaturas();*/
 		/*$query = $this->Curso_Model->get_all_students();*/
 		/*echo "<pre>";
 		print_r($query);
 		die();*/
-		$this->load->view("docente/main");
+		$this->load->view("docente/Info_curso");
 		$this->load->view('footer');
 	}
 }
---
+++ application/views/docente/info_curso.php
@@ -0,0 +1,68 @@
+
+<style>
+.contenedor{
+	width: 850px;
+	margin: 0 auto;
+	border: 2px solid #000;
+	
+}
+.titulo-principal h1{
+	margin-top: 0;
+	background-color: #000;
+	color: #fff;
+	font-size: 20px;
+	padding-left: 10px;
+}
+.lista-opciones{
+	width: 150px;
+/* display: inline-block; */
+	background-color: bisque;
+	float: left;
+	margin-right: 10px;
+	}
+.Contenedor-general{
+	display: inline-block;
+	width: 640px;
+	border: 2px solid grey;
+	background-color: aquamarine;}
+.Contenedor-de-alumnos{
+	margin-right: 450px;
+	margin-left: 10px;
+	background: red;
+}
+
+</style>
+
+<div class = "contenedor"> 
+	<div class = "titulo-principal">
+		<span></span>
+		<h1> INFORMACION DEL CURSO:</h1>
+	</div>
+	<div style ="padding:20px">
+		<div class = "lista-opciones">
+			<li> Alumnos</li>
+			<li>Materias</li>
+			<li> Alumnos</li>
+			<li>Materias</li>
+		</div>	
+		<div class ="Contenedor-general">
+			<div class ="Titulo-de-opciones">
+				<span></span>
+				<h3>Alumnos</h3>
+			</div>
+			<div class ="Contendor-de-alumnos">
+				<ul>
+					<li>
+						<div>
+							<h2> Bechi 
+							</h2>
+							<h3> Diego David 
+							</h3>
+							<img src="">
+						</div>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
