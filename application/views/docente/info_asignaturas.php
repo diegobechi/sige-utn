d55802f0c254c746cf6974fb06bd96e187c78840
fersige <fernandoromero99@hotmail.com>
2014-09-27 09:11:52 -0300

    .

---
+++ application/views/docente/info_asignaturas.php
@@ -0,0 +1,96 @@
+<style>
+.contenedor-principal{
+	display: inline-block;
+	width: 740px;
+	height: 250px;
+	border: 2px solid grey;
+	margin: 0;
+	background-color: gainsboro;
+}
+.Titulo-principal h1{
+	background-color:orange;
+	color:black;
+	margin-top: 0px;
+	font-size: 20px;
+}
+.contenedor-asignaturas{
+	margin-top: 0px;
+	display: inline-block;
+  	background-color: darkslategrey;
+  	border: 2px solid grey;
+ 	width: 738px;
+ 	height: 200px;
+}
+.box-asignatura-generica{
+	background-color: white;
+	width: 150px;
+	display: inline-block;
+	border: 2px solid orange;
+
+}
+.box-asignatura-generica a{
+	text-align: center;
+
+}
+.box-asignatura{
+	text-decoration: none;
+}
+
+
+</style>
+<div class = "contenedor-principal">
+	<div class = "Titulo-principal">
+		<h1> <img src="img/book_1.png">  Asignaturas</h1>
+    </div>
+    	<div class = "contenedor-asignaturas">
+	     	<div class= " listado-asignaturas">
+	     		<ul>
+					<li class="box-asignatura-generica"> 
+						<a id="Asignatura"href="#" class ="box-asignatura">
+							<div>
+								<h3> Asignatura
+								</h3>
+								<img src="img/book_1.png">
+							</div>
+						</a>
+					</li>
+					<li class="box-asignatura-generica"> 
+						<a id="Asignatura"href="#" class ="box-asignatura">
+							<div>
+								<h3> Asignatura
+								</h3>
+								<img src="img/book_1.png">
+							</div>
+						</a>
+					</li>
+					<li class="box-asignatura-generica"> 
+						<a id="Asignatura"href="#" class ="box-asignatura">
+							<div>
+								<h3> Asignatura
+								</h3>
+								<img src="img/book_1.png">
+							</div>
+						</a>
+					</li>
+					<li class="box-asignatura-generica"> 
+						<a id="Asignatura"href="#" class ="box-asignatura">
+							<div>
+								<h3> Asignatura
+								</h3>
+								<img src="img/book_1.png">
+							</div>
+						</a>
+					</li>
+					<li class="box-asignatura-generica"> 
+						<a id="Asignatura"href="#" class ="box-asignatura">
+							<div>
+								<h3> Asignatura
+								</h3>
+								<img src="img/book_1.png">
+							</div>
+						</a>
+					</li>
+				</ul>
+	     </div> 
+    </div>
+</div>
