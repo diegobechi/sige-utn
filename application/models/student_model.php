8b679298c549b85da2bf29cb84d459ca3b1a4dd4
Diego Bechi <diegobechi@gmail.com>
2014-10-06 15:12:27 -0300

    reorganizando las cosas para darle fluidez

--- application/models/student_model.php
+++ application/models/student_model.php
@@ -32,7 +32,8 @@ class Student_Model extends CI_Model {
                                                   t.idDomicilio = d.idDomicilio and
                                                   t.idEstadoCivil = e.idEstado and
                                                   a.legajoAlumno= $legajoAlumno");
-      )
+        $query = $string_query->result();
+        return $this->clear_result($query);
     }
 
     function get_tutores($legajoAlumno){
@@ -47,7 +48,7 @@ class Student_Model extends CI_Model {
                                           FROM Alumno a, Tutor t, GrupoFamiliar gf
                                           WHERE a.legajoAlumno = gf.legajoAlumno and
                                           t.idTutor = gf.idTutor and
-                                          t.idTutor = $idTutor;")
+                                          t.idTutor = $idTutor;");
 
     }
 
