f13e3d8a40eabe22aba4091516e760c60cac7e5a
fersige <fernandoromero99@hotmail.com>
2014-12-20 13:04:14 -0300

    .

--- application/config/database.php
+++ application/config/database.php
@@ -49,8 +49,8 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-/*$db['default']['username'] = 'pc\pampa';*/
-$db['default']['username'] = 'Bechi\Diego';
+$db['default']['username'] = 'pc\pampa';
+/*$db['default']['username'] = 'Bechi\Diego';*/
 $db['default']['password'] = '';
 $db['default']['port'] 	   = '8080';
 $db['default']['database'] = 'SIGE_UTN';
