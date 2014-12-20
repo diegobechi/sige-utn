acceee5630e29c664f8e15816127ce36a3257581
fersige <fernandoromero99@hotmail.com>
2014-12-20 16:50:23 -0300

    .

--- application/views/header.php
+++ application/views/header.php
@@ -5,8 +5,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <title>Instituto Santa Teresita</title>
         <meta name="viewport" content="width=device-width">
-        <link rel="stylesheet" type="text/css" href="../css/style-home.css">
-        <!--<link rel="stylesheet" type="text/css" href="../css/style1.css">-->
+        <!--<link rel="stylesheet" type="text/css" href="../css/style-home.css">-->
+        <link rel="stylesheet" type="text/css" href="../css/style1.css">
         <link rel="stylesheet" type="text/css" href="../css/animations.css">
         <!--<link rel="stylesheet" type="text/css" href="../css/bootstrap.css">-->
        
