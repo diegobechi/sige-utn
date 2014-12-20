34584bcc8df1789398ceaa1bf2957acd41f5e4ac
Diego Bechi <diegobechi@gmail.com>
2014-12-20 16:56:25 -0300

    diego

--- application/views/footer.php
+++ application/views/footer.php
@@ -4,6 +4,8 @@
 		<script src="../js/libs/jquery.jscrollpane.min.js"></script>
 		<script src="../js/libs/highcharts.js"></script>
 		<script type="text/javascript" src="../js/script.js"></script>
+		<script type="text/javascript" src="../js/docente.js"></script>
+		<script type="text/javascript" src="../js/alumno.js"></script>
 
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 		<script src="//blueimp.github.io/Gallery/js/jquery.blueimp-gallery.min.js"></script>
